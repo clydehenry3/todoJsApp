6a67211366eb1737a787381db86f5966b24475db
Clyde Forland <clydehenrydesign@gmail.com>
2023-11-09 00:28:57 -0600

    Add new files and initial code for TodoJS app

---
+++ xCode/TodoJS/TodoJS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoJS
+//
+//  Created by Clyde Forland III on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xCode/TodoJS/TodoJS/TodoJSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoJSApp.swift
+//  TodoJS
+//
+//  Created by Clyde Forland III on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoJSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
